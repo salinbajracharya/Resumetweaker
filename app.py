85f0e530137cb551ae3c5f3c201553fa12ee7bd3
Salin Bajracharya <85172270+salinbajracharya@users.noreply.github.com>
2025-05-29 07:40:06 -0500

    Update app.py
    Line update

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, send_file, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 db = SQLAlchemy()
 login_manager = LoginManager()
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 import os
